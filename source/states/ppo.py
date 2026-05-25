0be437c3dd730f4377fb08168f639c782ddb3fd8
Pieter Stevens <pieter.stevens@icloud.com>
2026-05-25 18:26:48 +0200

    added forward_critic

--- source/states/ppo.py
+++ source/states/ppo.py
@@ -1,33 +1,6 @@
-import gymnasium as gym
-from gymnasium import spaces
-import numpy as np
-from stable_baselines3 import PPO
 from stable_baselines3.common.policies import ActorCriticPolicy
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from torch import nn
-
-
-class Env(gym.Env):
-    def __init__(self, width=20, height=15, num_features=7, num_frames=4,
-                 num_actions=10):
-        super().__init__()
-
-        self.observation_space = spaces.Box(
-            low=-1.0,
-            high=1.0,
-            shape=(num_features * num_frames, height, width),
-            dtype=np.float32,
-        )
-
-        self.action_space = spaces.Discrete(num_actions)
-
-    def reset(self, seed=None, options=None):
-        obs = self.get_obs()
-        info = {}
-        return obs, info
-
-    def step(self, action):
-        pass
 
 
 class MarioResidualBlock(nn.Module):
@@ -89,7 +62,13 @@ class MarioPPO(nn.Module):
         )
 
     def forward(self, x):
-        return self.policy_net(x), self.value_net(x)
+        return self.forward_actor(x), self.forward_critic(x)
+
+    def forward_actor(self, x):
+        return self.policy_net(x)
+    
+    def forward_critic(self, x):
+        return self.value_net(x)
 
 
 class MarioPPOWrapper(ActorCriticPolicy):
@@ -101,28 +80,3 @@ class MarioPPOWrapper(ActorCriticPolicy):
     
     def _build_mlp_extractor(self):
         self.mlp_extractor = MarioPPO(self.features_dim)
-
-
-env = Env()
-
-
-policy_kwargs = dict(
-    features_extractor_class=MarioEncoder,
-    features_extractor_kwargs=dict(features_dim=128),
-)
-
-
-model = PPO(
-    policy=MarioPPOWrapper,
-    env=env,
-    policy_kwargs=policy_kwargs,
-    learning_rate=0.0003,
-    n_steps=2_048,
-    batch_size=64,
-    n_epochs=10,
-    gamma=0.99,
-    verbose=1,
-)
-
-
-model.learn(total_timesteps=1_000_000)
